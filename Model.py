25ec746da8c3e45d961ed52459c17dd03f450ac0
Stylianos Ioannou <stylianos.ioannou@city.ac.uk>
2025-04-01 15:32:19 +0100

    I integrated the baseline model in Model.py using a pre-trained Faster R-CNN with a ResNet-50 FPN, and adapted the classification head for MS COCO 2017 (91 classes). I also added an 'if name == "main":' block to test that the model loads correctly when run directly.

--- Model.py
+++ Model.py
@@ -1,1 +1,14 @@
-print("Hello world")
+import torchvision
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+
+
+def faster_rcnn_model(num_classes = 91):
+    model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained = True)
+    in_features = model.roi_heads.box_predictor.cls_score.in_features
+    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+
+    return model
+
+if __name__ == "__main__":
+    model = faster_rcnn_model()
+    print("Model loaded successfully!")
